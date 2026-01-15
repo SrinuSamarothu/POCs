41fcd7c9bf9a61d385065c71a868a1d7ad4d920e
Srinu S <srinusamarothu@gmail.com>
2026-01-15 10:45:39 +0530

    Create .NET library package

---
+++ Library Packaging/Calculator/Calculator/Operations.cs
@@ -0,0 +1,19 @@
+﻿namespace Calculator
+{
+    public static class Operations
+    {
+        public static int Add(int a, int b) => a + b;
+
+        public static int Subtract(int a, int b) => a - b;
+
+        public static int Multiply(int a, int b) => a * b;
+
+        public static int Divide(int a, int b)
+        {
+            if(b == 0)
+                throw new System.DivideByZeroException("Denominator cannot be zero.");
+
+            return a / b;
+        }
+    }
+}
---
+++ Library Packaging/CalculatorApp/Program.cs
@@ -0,0 +1,10 @@
+﻿using Calculator;
+
+
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Addition: {Operations.Add(a, b)}");
+Console.WriteLine($"Subtraction: {Operations.Subtract(a, b)}");
+Console.WriteLine($"Multiplication: {Operations.Multiply(a, b)}");
+Console.WriteLine($"Division: {Operations.Divide(a, b)}");
