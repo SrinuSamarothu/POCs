9066ab6d4d9531ee06de403eacf89dd9d00e2d9e
Srinu S <srinusamarothu@gmail.com>
2026-03-14 13:18:51 +0530

    SignalR with docke container support

---
+++ SignalR (WebSocket)/SignalR/Program.cs
@@ -0,0 +1,38 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddSignalR();
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins("http://127.0.0.1:5500") 
+               .AllowAnyHeader()
+               .AllowAnyMethod()
+               .AllowCredentials();
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseCors();
+
+app.UseAuthorization();
+
+app.MapHub<ChatHub>("/chatHub");
+app.MapControllers();
+
+app.Run();
